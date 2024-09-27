f5a5565e607e910760885bf03e0666536758b516
Shreyas-jobtkk <g_shreyas@jobtkk.co.jp>
2024-09-27 10:15:27 +0900

    test2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
+
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hello, World2!');
+  res.send('Hello, World!');
 });
 
-const port = process.env.PORT || 4000
+// Use the port from Render's environment or default to 10000
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Zoom Meeting SDK Auth Endpoint Sample Node.js, listening on port ${port}!`);
 });
