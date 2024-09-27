b2b02f3639b0887df9dfca4266d10bc40b79b633
Shreyas-jobtkk <g_shreyas@jobtkk.co.jp>
2024-09-27 13:10:58 +0900

    test 6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,92 @@
-import express from 'express';
-const app = express();
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import { KJUR } from 'jsrsasign'
+import { inNumberArray, isBetween, isRequiredAllOrNone, validateRequest } from './validations.js'
+
+let db = [
+  {
+    "userId": "user123",
+    "languages": ["English"],
+    "status": "available",
+    "meetingNumber": "111111155",
+    "password": "eeeeee"
+  },
+  {
+    "userId": "user456",
+    "languages": ["English", "Portuguese", "French"],
+    "status": "busy",
+    "meetingNumber": "23456789014",
+    "password": "pass5678"
+  },
+  {
+    "userId": "user789",
+    "languages": ["Vietnamese"],
+    "status": "available",
+    "meetingNumber": "345678345678",
+    "password": "345678"
+  }
+]
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(express.json(), cors())
+app.options('*', cors())
+
+const propValidations = {
+  role: inNumberArray([0, 1]),
+  expirationSeconds: isBetween(1800, 172800)
+}
+
+const schemaValidations = [isRequiredAllOrNone(['meetingNumber', 'role'])]
+
+const coerceRequestBody = (body) => ({
+  ...body,
+  ...['role', 'expirationSeconds'].reduce(
+    (acc, cur) => ({ ...acc, [cur]: typeof body[cur] === 'string' ? parseInt(body[cur]) : body[cur] }),
+    {}
+  )
+})
 
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Use the port from Render's environment or default to 10000
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}!`);
+app.post('/', (req, res) => {
+  const requestBody = coerceRequestBody(req.body)
+  const validationErrors = validateRequest(requestBody, propValidations, schemaValidations)
+
+  if (validationErrors.length > 0) {
+    return res.status(400).json({ errors: validationErrors })
+  }
+
+  const { meetingNumber, role, expirationSeconds } = requestBody
+  const iat = Math.floor(Date.now() / 1000)
+  const exp = expirationSeconds ? iat + expirationSeconds : iat + 60 * 60 * 2
+  const oHeader = { alg: 'HS256', typ: 'JWT' }
+
+  const oPayload = {
+    appKey: process.env.ZOOM_MEETING_SDK_KEY,
+    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+    mn: meetingNumber,
+    role,
+    iat,
+    exp,
+    tokenExp: exp
+  }
+
+  const sHeader = JSON.stringify(oHeader)
+  const sPayload = JSON.stringify(oPayload)
+  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
+  return res.json({ signature: sdkJWT, data:db })
+})
+
+// Route to get the db data
+app.get('/api/users', (req, res) => {
+  res.json(db);
 });
+
+app.listen(port, () => console.log(`Zoom Meeting SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
