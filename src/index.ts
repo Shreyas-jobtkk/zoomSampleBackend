5dfa285cef5c384513c182a32d548ad827b0d184
Shreyas-jobtkk <g_shreyas@jobtkk.co.jp>
2025-01-23 15:05:33 +0900

    removed owasp

--- src/index.ts
+++ src/index.ts
@@ -35,20 +35,20 @@ app.use(
   })
 );
 
-// Middleware to set security headers
-app.use((req, res, next) => {
-  res.setHeader(
-    "Strict-Transport-Security",
-    "max-age=31536000; includeSubDomains; preload"
-  );
-  res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self';"
-  );
-  res.setHeader("Referrer-Policy", "no-referrer-when-downgrade");
-  next();
-});
+// // Middleware to set security headers
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Strict-Transport-Security",
+//     "max-age=31536000; includeSubDomains; preload"
+//   );
+//   res.setHeader("X-Content-Type-Options", "nosniff");
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'self'; script-src 'self';"
+//   );
+//   res.setHeader("Referrer-Policy", "no-referrer-when-downgrade");
+//   next();
+// });
 
 // app.post("/api/terminalActivity", async (req, res) => {
 //   const { terminal_id, personStatus } = req.body;
